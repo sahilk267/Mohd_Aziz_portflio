013e89c11fd401a27ea9b18bfaf67867c080da4e
Admin <admin@Admins-MacBook-Pro.local>
2026-06-30 03:55:05 +0530

    Fix: Remove basePath for deploy-pages action, add .nojekyll

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/Mohd_Aziz_portflio',
-  assetPrefix: '/Mohd_Aziz_portflio/',
   images: {
     unoptimized: true,
   },
